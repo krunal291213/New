3034d181bf16b5916a523f33f91d055f732047e7
krunal291213 <patelkrunal291213@gmail.com>
2025-10-30 02:48:25 +0530

    Update user IDs and retain existing code structure

--- vars.py
+++ vars.py
@@ -11,11 +11,12 @@ OWNER = int(environ.get("OWNER", "7425217769"))
 CREDIT = environ.get("CREDIT", "Admin")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7425217769').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7425217769').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
